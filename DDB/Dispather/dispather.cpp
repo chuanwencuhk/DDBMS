0282663659fe6ba374ccf0bf3413e693be2061e0
YF-CHAI <chai-leon@qq.com>
2018-01-08 16:21:08 +0800

    2018.1.8

--- DDB/Dispather/dispather.cpp
+++ DDB/Dispather/dispather.cpp
@@ -2,5 +2,15 @@
 
 Dispather::Dispather()
 {
+    if(pmtr == NULL)
+        pmtr = MetadataManager::getInstance();
+
+}
+
+void Dispather::set_treevector_from_querytree(struct query_tree& qtree)
+{
+    int root_pos = qtree.root;
+
+
 
 }
--- DDB/Dispather/dispather.h
+++ DDB/Dispather/dispather.h
@@ -3,24 +3,30 @@
 
 #include "global.h"
 #include "../MetadataManager/metadatamanager.h"
-
+#include "../QueryTree/query_tree.h"
+#include <set>
+#include <vector>
 
 //to use LHH's RPC interface
 extern bool RPCExecute(string host_ip, string sql_statement);
-
 extern string RPCExecuteQuery(string host_ip, string sql_statement);//use for exec sql
-
 extern int RPCExecuteUpdate(string host_ip, string sql_statement);
-
 extern bool RPCInsertFileToTable(string host_ip, string sql_file, string table_name);
+
+//to use WHY's query tree
+extern struct query_tree get_original_tree(string s);
 
 class Dispather
 {
 public:
     Dispather();
+    void set_treevector_from_querytree(struct query_tree& qtree);
 
 private:
     MetadataManager* pmtr;
+
+    set<std::string> tree_set;
+    vector<struct treenode> tree_vector;
 
 };
 
